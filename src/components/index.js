52ac12b8d05fafda64ccedfe3e90349841904334
Nishant-96 <nishant11pratihast@gmail.com>
2022-04-12 02:54:44 +0530

    feat: barrel export

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { Navbar } from "./navbar/navbar.jsx";
+export { Sidenav } from "./sidenav/sidenav.jsx";
+export { CategoryCard } from "./category-card/category-card.jsx";
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { Home } from "./home/home.jsx";
+export { Login } from "./authentication/login.jsx";
+export { Forgot } from "./authentication/forgot.jsx";
+export { SignUp } from "./authentication/signup.jsx";
