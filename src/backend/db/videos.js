dd95f885623236ffa6c1f40ed018457165239dcf
Nishant-96 <nishant11pratihast@gmail.com>
2022-04-13 05:41:12 +0530

    update: database setup

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,17 +5,137 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    _id: "MJiBnPRD298",
+    title: "Moon Knight",
+    category: "Marvel",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+      "Experience the Marvel universe like you’ve never seen. Watch Marvel Studios’ Moon Knight, now streaming on Disney+.",
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: "frdj1zb9sMY",
+    title: "Rogue One: A Star Wars Story",
+    category: "Star Wars",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "Research scientist Galen Erso and his family are in hiding on the planet Lah'mu when Imperial weapons developer Orson Krennic arrives to press him into completing the Death Star, a space station-based superweapon capable of destroying entire planets. ",
+  },
+  {
+    _id: "aWzlQ2N6qqg",
+    title: "Doctor Strange in the Multiverse of Madness",
+    category: "Marvel",
+    description:
+      "In Marvel Studios Doctor Strange in the Multiverse of Madness, the MCU unlocks the Multiverse and pushes its boundaries further than ever before. Journey into the unknown with Doctor Strange, who, with the help of mystical allies both old and new, traverses the mind-bending and dangerous alternate realities of the Multiverse to confront a mysterious new adversary.",
+  },
+  {
+    _id: "cM53X5Nqjbc",
+    title: "Star Wars: Return Of The Jedi",
+    category: "Star Wars",
+    description:
+      "Luke struggles to help Vader back from the dark side. Meanwhile, the rebels dispatch to Endor to destroy a more powerful Death Star.",
+  },
+  {
+    _id: "rt-2cxAiPJk",
+    title: "SPIDER-MAN: NO WAY HOME",
+    category: "Marvel",
+    description:
+      "For the first time in the cinematic history of Spider-Man, our friendly neighborhood hero is unmasked and no longer able to separate his normal life from the high-stakes of being a Super Hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.",
+  },
+  {
+    _id: "frdj1zb9sMY",
+    title: "Rogue One: A Star Wars Story",
+    category: "Star Wars",
+    description:
+      "Research scientist Galen Erso and his family are in hiding on the planet Lah'mu when Imperial weapons developer Orson Krennic arrives to press him into completing the Death Star, a space station-based superweapon capable of destroying entire planets. ",
+  },
+  {
+    _id: "7SlILk2WMTI",
+    title: "Captain America: The Winter Soldier",
+    category: "Marvel",
+    description:
+      "The First Avenger returns for an all-new cinematic adventure April 4 in Marvel's Captain America: The Winter Soldier!",
+  },
+  {
+    _id: "Ke1Y3P9D0Bc",
+    title: "Iron Man 3",
+    category: "Marvel",
+    description:
+      "Marvel's Iron Man 3 pits brash-but-brilliant industrialist Tony Stark/Iron Man against an enemy whose reach knows no bounds. When Stark finds his personal world destroyed at his enemy's hands, he embarks on a harrowing quest to find those responsible. This journey, at every turn, will test his mettle.",
+  },
+  {
+    _id: "2BkVf2voCr0",
+    title: "Home Sweet Home Alone",
+    category: "Disney",
+    description:
+      "Home Sweet Home Alone stars Ellie Kemper, Rob Delaney, Archie Yates, Aisling Bea, Kenan Thompson, Tim Simons, Pete Holmes, Devin Ratray, Ally Maki, and Chris Parnell. The film is directed by Dan Mazer from a screenplay by Mikey Day & Streeter Seidell, story by Mikey Day & Streeter Seidell and John Hughes based on a screenplay by John Hughes.",
+  },
+  {
+    _id: "zL4LFSbFPcw",
+    title: "The Batman",
+    category: "Disney",
+    description:
+      "On Halloween, Gotham City mayor Don Mitchell Jr. is murdered by a man calling himself the Riddler. Reclusive billionaire Bruce Wayne, who has operated for two years as the vigilante Batman, investigates alongside the Gotham City Police Department (GCPD). At the crime scene, lieutenant James Gordon discovers a message that the Riddler left for Batman. Soon after, the Riddler kills commissioner Pete Savage and leaves another message for Batman.",
+  },
+  {
+    _id: "T6DJcgm3wNY",
+    title: "Superman-Man of Steel",
+    category: "Disney",
+    description:
+      "The planet Krypton is destabilized from the mining of the planetary core. Just before the planet explodes, Krypton's supreme council chief advisor Jor-El infuses the genetics codex into his infant son, Kal-El, the first naturally born Kryptonian child in centuries. Jor-El manages to send Kal-El in a spacecraft toward Earth before being killed by General Zod during an uprising.",
+  },
+  {
+    _id: "7-GO9fo9DtM",
+    title: "Green Lantern",
+    category: "Disney",
+    description:
+      "Hal Jordan, a cocky test pilot working at Ferris Aircraft, is chosen by the ring and transported to the crash site, where the dying Abin Sur appoints him a Green Lantern, telling him to take the lantern and speak the oath. Jordan says the oath and is whisked away to the Green Lantern Corps home planet of Oa, where he meets and trains with veteran Corps members Tomar-Re, Kilowog, and Corps leader Sinestro, who believes he is unfit and fearful",
+  },
+  {
+    _id: "2LeOH9AGJQM",
+    title: "Cars 3",
+    category: "Pixar",
+    description:
+      "In a world populated by anthropomorphic vehicles, the final race of the Piston Cup begins a rivalry between retiring veteran Strip 'The King' Weathers, frequent runner-up Chick Hicks, and rookie sensation Lightning McQueen. At the back of the field, McQueen avoids a multi-car crash caused by Chick. ",
+  },
+  {
+    _id: "drQWopZDEEY",
+    title: "The Flash",
+    category: "Disney",
+    description:
+      "Crime-scene investigator Barry Allen who gains super-human speed after the explosion of the S.T.A.R. Labs' particle accelerator which he uses to fight crime and hunt other metahumans in Central City as the Flash, a masked superhero. Barry eventually discovers that his mentor Harrison Wells is actually Eobard Thawne, his archenemy from the future who murdered his mother Nora Allen when he was a child.",
+  },
+
+  {
+    _id: "pqdHP2dWQ9M",
+    title: "Turning Red",
+    category: "Pixar",
+    description:
+      "Growing up is a beast. Check out the new trailer for the upcoming all-new original feature film “Turning Red” from Pixar Animation Studios.",
+  },
+  {
+    _id: "apx1aUP3MqA",
+    title: "Burrow",
+    category: "Pixar",
+    description:
+      "A young rabbit embarks on a journey to dig the burrow of her dreams, despite not having a clue what she’s doing.",
+  },
+  {
+    _id: "BsOmYpP4UDU",
+    title: "The Bad Batch",
+    category: "Star Wars",
+    description:
+      "Clone Force 99, also known as the Bad Batch—a group of elite clone troopers with genetic mutations that were first introduced in Star Wars: The Clone Wars—take on daring mercenary missions in the aftermath of the Clone Wars.",
+  },
+  {
+    _id: "BwZs3H_UN3k",
+    title: "Buzz Lightyear",
+    category: "Pixar",
+    description:
+      "Lightyear revolves around the origin story of the original Buzz Lightyear, the character who inspired the action figure in the Toy Story films. In this film, Buzz does his first test flight in order to explore new galaxies for Star Command to come in peace. ",
+  },
+  {
+    _id: "aOC8E8z_ifw",
+    title: "The Mandalorian",
+    category: "Star Wars",
+    description:
+      "After the stories of Jango and Boba Fett, another warrior emerges in the Star Wars universe. “The Mandalorian” is set after the fall of the Empire and before the emergence of the First Order.",
   },
 ];
