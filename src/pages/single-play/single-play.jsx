aea01d1b8649e08b625aac2b97385feb45c66d82
Nishant-96 <nishant11pratihast@gmail.com>
2022-04-15 22:53:24 +0530

    feat: single video page

--- src/pages/single-play/single-play.jsx
+++ src/pages/single-play/single-play.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
@@ -6,24 +7,29 @@ import BookmarkAddIcon from "@mui/icons-material/BookmarkAdd";
 
 import "./single-play.css";
 import { Sidenav } from "../../components";
+import { useData } from "../../context/data-context";
 
 export function SinglePlay() {
+  const { videoId } = useParams();
+  const { state } = useData();
+  const [video] = state.defaultVideos.filter((curr) => curr._id === videoId);
+
   return (
     <div className="explore">
       <Sidenav />
       <div className="single-play-listing">
         <iframe
           className="sp-iframe"
-          src={`https://www.youtube.com/embed/UNnKAk79wws`}
+          src={`https://www.youtube.com/embed/${videoId}`}
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen=""
         ></iframe>
         <div className="sp-listing-header">
-          <h2>Video Title</h2>
-          <h5>Video Category</h5>
-          <p>Description</p>
+          <h2>{video.title}</h2>
+          <h5>{video.category}</h5>
+          <p>{video.description}</p>
           <div className="sp-icons">
             <div className="sp-icon-sec">
               <ThumbUpIcon />
